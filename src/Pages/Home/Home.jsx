cab525202d936e66d9c90a7f2eef22548f821e76
Maciej Krogulski <maciejkrogulski1@gmail.com>
2023-05-08 19:00:23 +0200

    mainly css updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,14 +8,22 @@ const Home = ({ loadTrendingsMovies, movies, loadMovieDetails }) => {
   }, []);
 
   return (
-    <div>
-      <p>Home-page</p>
-      <ul>
+    <div className='movies__container'>
+      <h1 className='home-slogan'>Trending movies</h1>
+      <ul className="movie__list">
         {movies.map(movie => {
+          console.log(movie);
           return (
-            <li key={movie.id}>
+            <li key={movie.id} className="movie__card">
               <Link to={`movie/${movie.id}`} state={{ from: '/' }}>
-                {movie.original_title}
+                  <img
+                    src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.original_title}
+                    className='movie__img'
+                  />
+                <div className="movie__overlay">
+                  <h3>{movie.original_title}</h3>
+                </div>
               </Link>
             </li>
           );
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,41 @@
 import { Outlet, NavLink } from 'react-router-dom';
 import { Suspense } from 'react';
-import css from './SharedLayout.module.css'
 
 export const SharedLayout = () => {
   return (
-    <div className={css.container}>
-      <header className={css.header}>
-        <nav className={css.nav}>
-          <NavLink className={css.navLink} to="/">Home</NavLink>
-          <NavLink className={css.navLink} to="/movie">Movie</NavLink>
-        </nav>
+    <div className="all__container">
+      <header>
+        <div className="header__content">
+          <div className='name__container'>
+            <svg
+            className='icon'
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="512"
+              height="512"
+              viewBox="0 0 512 512"
+            >
+              <g id="icomoon-ignore"></g>
+              <path d="M0 64v384h512v-384h-512zM96 416h-64v-64h64v64zM96 288h-64v-64h64v64zM96 160h-64v-64h64v64zM384 416h-256v-320h256v320zM480 416h-64v-64h64v64zM480 288h-64v-64h64v64zM480 160h-64v-64h64v64zM192 160v192l128-96z"></path>
+            </svg>
+            <h2 className="name">MOVIEWEB</h2>
+          </div>
+          <nav className="nav">
+            <NavLink className="nav__link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav__link" to="/movie">
+              Movie
+            </NavLink>
+          </nav>
+        </div>
       </header>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
+      <div className="box"></div>
+      <div className="outlet__container">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </div>
     </div>
   );
 };
